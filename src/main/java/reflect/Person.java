fb9ab51ae5cf81295e5251595564574be7096a24
qintingshuang <935501036@qq.com>
2019-11-09 15:46:00 +0800

    反射机制，code 创建实例，调用方法和属性

---
+++ src/main/java/reflect/Person.java
@@ -0,0 +1,40 @@
+package reflect;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: qintingshuang
+ * @date: 2019/11/9 12:14
+ * @description:
+ */
+@Slf4j
+public class Person {
+
+    private String Name;
+
+    private Integer age;
+
+    private String hobby = "basketball";
+
+
+    public Person() {
+        log.info("实例化了共有的Person对象");
+    }
+
+    private Person(Integer age) {
+        this.age = age;
+        log.info("age:"+this.age);
+        log.info("实例化了私用的Person对象");
+
+    }
+
+    private String getName() {
+        return this.Name;
+    }
+
+    private void setName(String name) {
+        this.Name = name;
+    }
+
+
+}
---
+++ src/main/java/singleton/EnumSingleton.java
@@ -0,0 +1,35 @@
+package singleton;
+
+/**
+ * @author: qintingshuang
+ * @date: 2019/11/9 12:26
+ * @description: 枚举单例
+ */
+public enum EnumSingleton {
+    /**
+     * 创建单例对象QINTINGSHUANG，类型为EnumSingleton
+     */
+    QINTINGSHUANG
+
+
+/**
+ * 枚举单例的原理
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+
+
+
+
+
+
+}
