9955e94510ef9e1aa705cc5824d4dc7fe94c20fd
kevin <935501036@qq.com>
2020-02-01 22:27:00 +0800

    高级泛型运用

---
+++ src/main/java/generics/GenericsClass.java
@@ -0,0 +1,42 @@
+package generics;
+
+/**
+ *
+ * 泛型，是1.5之后出现的一种安全机制，它将运行出现的ClassCastException异常,转移到了编译期，方便寻找问题。
+ *
+ * 也避免强制转换问题
+ *
+ * 泛型类
+ * @author qintingshuang
+ * @date 2020/2/1 10:27
+ */
+public class GenericsClass {
+
+    public  static void main(String[]  args){
+        Tool tool=new Tool();
+        tool.setPerson(new Student());
+        tool.getPerson();
+
+        Tool tool1=new Tool();
+        tool1.setPerson(new Teacher());
+        tool1.getPerson();
+    }
+}
+
+
+
+class Tool<T>{
+
+    private  T person;
+
+    public void setPerson(T person){
+        this.person=person;
+    }
+    public T getPerson(){
+        return  person;
+    }
+}
+
+class Student{}
+
+class Teacher{};
